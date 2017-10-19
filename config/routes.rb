a8cb5a641e6db9f3b3f11328f0f26e61e98d0eaa
Joaquim Magalhaes <magalhaes1915@gmail.com>
2017-10-19 01:01:21 +0100

    Remove blank lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
   get '/logout' => 'sessions#destroy'
-
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-
   get '/dashboard' => 'dashboard#index'
   get '/dashboard/new' => 'dashboard#new'
-
   get '/dashboard/show' => 'dashboard#show'
-
   delete '/dashboard/show' => 'dashboard#delete'
-
   post '/dashboard/create' => 'dashboard#create'
-
-
 end
