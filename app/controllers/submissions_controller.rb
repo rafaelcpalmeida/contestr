64633123ab8e29353a4fa08693d5e5303daf3eb8
Joaquim Marcelo Moreira Magalhães <magalhaes1915@gmail.com>
2018-01-12 00:06:31 +0000

    Add last updates

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,6 +2,7 @@ class SubmissionsController < ApplicationController
   before_action :authorize
   require 'FileUtils'
   layout 'dashboard'
+  include SubmissionsHelper
 
   def new
     @project = Project.find(params[:project_id])
@@ -10,10 +11,20 @@ class SubmissionsController < ApplicationController
 
   def upload
     project_id = params[:submission][:project_id]
+    file_name = params[:submission][:attachment].original_filename
+    base_path = `pwd`
+    file_path = "#{base_path.strip}/public/uploads/#{project_id}/#{current_user.id}/#{file_name}"
+
     @submissions = Submission.new(user_id: current_user.id,
                                   project_id: project_id,
                                   attachment: params[:submission][:attachment])
     if @submissions.save
+      Thread.new do
+        analyze_process file_path, 8, 4, 4
+
+        # TODO: store the return of the method above on the database
+      end
+
       redirect_to '/dashboard'
     else
       redirect_to "/submissions/new?project_id=#{project_id}"
